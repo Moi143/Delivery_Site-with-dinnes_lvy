55cc41a2c35413b171fb259f1ba5b7405b863ecd
Mayur kumar bhure <53428039+Moi143@users.noreply.github.com>
2020-05-11 18:11:29 +0530

    Create App urls and connect with main urls.py
    Create same pages and urls.py file create d and we are  connected to main urls.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def home(request):
+    return HttpResponse('this is my home page')
+
+def about(request):
+    return HttpResponse("this is my about page and my name is mayur kumar bhure")
+
+def customer(request):
+    return HttpResponse("this is my about page and my name is mayur kumar bhure")
