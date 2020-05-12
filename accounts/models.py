dd865517eef27f11c24274a198da97ab7802c3b9
Mayur kumar bhure <53428039+Moi143@users.noreply.github.com>
2020-05-12 17:25:38 +0530

    all makemigrations
    products
    order
    customer

--- accounts/models.py
+++ accounts/models.py
@@ -13,14 +13,28 @@ class Customer(models.Model):
 
 
 class Product(models.Model):
+    CATEGORY = (
+        ("Indoor","Indoor"),
+        ('Out Door','Out Door'),
+    )
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField(null=True)
-    category = models.CharField(max_length=200, null=True)
-    description = models.CharFeild(max_length=200, null=True) 
+    category = models.CharField(max_length=200, null=True, choices=CATEGORY)
+    description = models.CharField(max_length=200, null=True) 
     date_created = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
         return self.name
 
 
-# class Order(models.Model):
+class Order(models.Model):
+    STATUS = (
+        ('pending','pending'),
+        ('Out of Deilvery','Out of Deilvery'),
+        ('Delivered','Delivered'),
+    )
+    # customer = models.
+    # products = models.
+    date_created = models.DateField(auto_now_add=True, null=True)
+    status = models.CharField(max_length=200,choices=STATUS)
+
