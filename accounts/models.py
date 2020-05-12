5eb1d91bda329f467220f0378865b54f97b346f9
Mayur kumar bhure <53428039+Moi143@users.noreply.github.com>
2020-05-12 16:50:10 +0530

    Creearte Customer Table
    This is customer table use thi sqlite3 dataa base

--- accounts/models.py
+++ accounts/models.py
@@ -2,3 +2,8 @@ from django.db import models
 
 # Create your models here.
 
+class Customer(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    phone = models.CharField(max_length=200)
+    email = models.CharField(max_length=200,blank=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
