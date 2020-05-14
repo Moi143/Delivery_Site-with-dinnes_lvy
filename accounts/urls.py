bdc5595ec86e011d669a1b8cc8cae1e6fb1faa93
Mayur kumar bhure <53428039+Moi143@users.noreply.github.com>
2020-05-14 10:41:54 +0530

    Create Order in dashborad
    views.py changes
    
    Create order_from.html

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,6 @@ from .import views
 urlpatterns = [
     path('',views.home,name='home'),
     path('products/',views.products,name='products'),
-    path('customer/<str:pk_text>/',views.customer,name='customer'),
+    path('customer_profile/<str:pk_text>/',views.customer,name='customer'),
+    path('create_order',views.create_order,name="Create_Order"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
+from .forms import OrderForm
 # Create your views here.
 
 def home(request):
@@ -12,7 +13,7 @@ def home(request):
     total_delivered = orders_data.filter(status='Delivered').count()
     total_pending = orders_data.filter(status='pending').count()
     
-    context = {'orders_show':orders_data,'customer_show':customer_data,'total_orders_show':total_orders,'order_delivered_show':total_delivered,'total_pending_show':total_pending}
+    context = {'orders_show':orders_data,'customer_show':customer_data,'total_orders_show':total_customers,'order_delivered_show':total_delivered,'total_pending_show':total_pending}
     return render(request, 'index.html',context)
 
 def products(request):
@@ -25,3 +26,16 @@ def customer(request, pk_text):
     total_orders = orders.count()
     context = {'customer':customer,'orders':orders,'order_counts':total_orders}
     return render(request, 'customers.html',context)
+
+def create_order(request):
+    from_data = OrderForm()
+
+    if request.method == 'POST':
+        # print('printing POST:', request.POST)
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'from_data':from_data}
+    return render(request,'order_form.html',context)
