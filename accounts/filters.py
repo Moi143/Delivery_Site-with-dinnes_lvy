a76adbefbce9e388dd00d542f1aac4de866d8c2a
Mayur kumar bhure <53428039+Moi143@users.noreply.github.com>
2020-05-17 12:02:05 +0530

    this inputext
    notes = CharFilter(field_name = "notes", lookup_expr='icontains',widget=TextInput(attrs={'placeholder': 'Notes'}))

--- accounts/filters.py
+++ accounts/filters.py
@@ -1,11 +1,11 @@
 import django_filters
-from django_filters import DateFilter, CharFilter
+from django_filters import CharFilter, DateFilter
 from .models import *
-
+from django.forms.widgets import TextInput
 class OrderFilter(django_filters.FilterSet):
-    start_date = DateFilter(field_name = "date_created" , lookup_expr='gte')
-    end_date = DateFilter(field_name = "date_created" , lookup_expr='lte')
-    notes = CharFilter(field_name = "notes", lookup_expr='icontains')
+    start_date = DateFilter(field_name = "date_created" , lookup_expr='gte',widget=TextInput(attrs={'placeholder': 'Month/Date/Year'}))
+    end_date = DateFilter(field_name = "date_created" , lookup_expr='lte',widget=TextInput(attrs={'placeholder': 'Month/Date/Year'}))
+    notes = CharFilter(field_name = "notes", lookup_expr='icontains',widget=TextInput(attrs={'placeholder': 'Notes'}))
     class Meta:
         model = Order
         fields = "__all__"
