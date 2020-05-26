21fdde532203d3ae1148be39123fab22c197b75a
Mayur kumar bhure <53428039+Moi143@users.noreply.github.com>
2020-05-26 19:35:35 +0530

    decorators
    if request.user.is_authenticated:
            return redirect('home')
    
    and
    
    using
    
    from django.contrib.auth.decorators import login_required

--- accounts/views.py
+++ accounts/views.py
@@ -7,38 +7,46 @@ from .forms import OrderForm, CreateUserForm
 from .filters import OrderFilter
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def registerpage(request):
-    form = CreateUserForm()
-    if request.method == 'POST':
-        form = CreateUserForm(request.POST)
-        if form.is_valid():
-            form.save()
-            first_name = form.cleaned_data.get('first_name')
-            last_name = form.cleaned_data.get('last_name')
-            messages.success(request, 'Account is created for ' + first_name + " " + last_name )
-            return redirect('login')
-    context = {'form':form}
-    return render(request, 'register14.html', context)
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form = CreateUserForm()
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                first_name = form.cleaned_data.get('first_name')
+                last_name = form.cleaned_data.get('last_name')
+                messages.success(request, 'Account is created for ' + first_name + " " + last_name )
+                return redirect('login')
+        context = {'form':form}
+        return render(request, 'register14.html', context)
 
 def loginpage(request):
-    if request.method == 'POST':
-        username1 = request.POST.get('username')
-        password1 = request.POST.get('password')
-        user = authenticate(request, username=username1 , password=password1)
-        if user is not None:
-            login(request, user)
-            return redirect('home')
-        else:
-            messages.info(request,'Username or Password is not Valid')
-    context = {}
-    return render(request,'login14.html',context)
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            username1 = request.POST.get('username')
+            password1 = request.POST.get('password')
+            user = authenticate(request, username=username1 , password=password1)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.info(request,'Username or Password is not Valid')
+        context = {}
+        return render(request,'login14.html',context)
 
 def logoutuser(request):
     logout(request)
     return redirect('login')
 
+@login_required(login_url='login') #decorators
 def home(request):
     orders_data = Order.objects.all()
     customer_data = Customer.objects.all()
@@ -49,10 +57,12 @@ def home(request):
     context = {'orders_show':orders_data,'customer_show':customer_data,'total_orders_show':total_customers,'order_delivered_show':total_delivered,'total_pending_show':total_pending}
     return render(request, 'index.html',context)
 
+@login_required(login_url='login') #decorators
 def products(request):
     products_data = Product.objects.all()
     return render(request, 'products.html',{'products_show':products_data})
 
+@login_required(login_url='login') #decorators
 def customer(request, pk_text):
     customer = Customer.objects.get(id=pk_text)
     orders = customer.order_set.all()
@@ -62,6 +72,7 @@ def customer(request, pk_text):
     context = {'customer':customer,'orders':orders,'order_counts':total_orders,'myfilter':myfilter}
     return render(request, 'customers.html',context)
 
+@login_required(login_url='login') #decorators
 def create_order(request, pk):
     OrderFormSet = inlineformset_factory(Customer, Order, fields=("products", 'status'),extra=10)
     customer = Customer.objects.get(id=pk)
@@ -74,6 +85,7 @@ def create_order(request, pk):
     context = {'fromset':fromset}
     return render(request,'order_form.html',context)
 
+@login_required(login_url='login') #decorators
 def update_order(request, pk1):
     u_order = Order.objects.get(id=pk1)
     form_update = OrderForm(instance=u_order)
@@ -85,6 +97,7 @@ def update_order(request, pk1):
     context = {'from':form_update}
     return render(request, 'order_form.html' ,context)
 
+@login_required(login_url='login') #decorators
 def delete(request, pk2):
     d_order = Order.objects.get(id=pk2)
     if request.method == 'POST':
