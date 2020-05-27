3da3425d2b66f570f9ae2664ecd1f2d08a98a17a
Mayur kumar bhure <53428039+Moi143@users.noreply.github.com>
2020-05-27 15:18:34 +0530

    coustomer login with the help of decorator

--- accounts/views.py
+++ accounts/views.py
@@ -9,6 +9,7 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from .decorator import unauthenticated_user, allowed_users, admin_only
+from django.contrib.auth.models import Group
 
 # Create your views here.
 @unauthenticated_user
@@ -17,9 +18,13 @@ def registerpage(request):
     if request.method == 'POST':
         form = CreateUserForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
             first_name = form.cleaned_data.get('first_name')
             last_name = form.cleaned_data.get('last_name')
+
+            group = Group.objects.get(name='customer')
+            user.groups.add(group)
+            
             messages.success(request, 'Account is created for ' + first_name + " " + last_name )
             return redirect('login')
     context = {'form':form}
